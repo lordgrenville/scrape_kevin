48e133a9d1eb2f63de36695ca1169ba981f2fde3
Josh Friedlander <16547083+lordgrenville@users.noreply.github.com>
2021-10-28 16:13:26 +0300

    add new posts in order

--- scrape_kevin.py
+++ scrape_kevin.py
@@ -7,6 +7,7 @@ from bs4 import BeautifulSoup
 
 ROOT = "https://www.nationalaffairs.com"
 
+
 def get_latest_articles():
     r_root = requests.get(ROOT + "/authors/detail/kevin-lewis")
     soup_root = BeautifulSoup(r_root.text, "html.parser")
@@ -21,7 +22,7 @@ def htmlencode(text):
 
 
 def create_feed(title, link, title_di):
-    feed = []
+    feed = ''
     feed += (
         '<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:dc="http://purl.org/dc/elements/1.1/"><channel>\n  <title>'
         + htmlencode(title)
@@ -32,40 +33,49 @@ def create_feed(title, link, title_di):
         + '<atom:link href="localhost:8888/rss.xml" rel="self" type="application/rss+xml" />'
     )
     for item_title, item_link in title_di.items():
-        insert_item(feed, item_link, item_title)
+        feed += insert_item('', item_link, item_title)
     feed += "</channel></rss>"
     return "".join(feed)
 
 
-def insert_item(feed, item_link, item_title):
-    if item_link in feed:
-        print(f"item {item_title} is already in the feed")
-        return feed
+def get_item(item_link, item_title):
+    print(f'New post: {item_title}')
     soup = BeautifulSoup(requests.get(item_link).text, "html.parser")
     soup.find("div", {"class": "article-social-bar"}).clear()
-    # find first item and insert just before
-    ind = feed.find('<item>')
     post = "  <item>"
     post += f"  <title>{item_title} </title>"
     post += f"  <link>{ROOT + item_link} </link>"
     post += "  <description>"
     post += htmlencode(str(soup.find("div", attrs={"class": "article-content"})))
     post += "</description>  </item>"
-    feed = feed[:ind] + post + feed[ind:]
     sleep(5)
+    return post
+
+
+def insert_item(feed, item):
+    # find first item and insert just before
+    ind = feed.find("<item>")
+    feed = feed[:ind] + item + feed[ind:]
     return feed
 
 
 titles_di = get_latest_articles()
 try:
     with open("kevin_lewis.rss", "r") as f:
-        new_feed = f.read()
+        existing_feed = f.read()
+        new_items = []
         for item_title_, item_link_ in titles_di.items():
-            new_feed = insert_item(new_feed, item_link_, item_title_)
+            if item_link_ in existing_feed:
+                print(f"item {item_title_} is already in the feed")
+                continue
+            new_items.append(get_item(item_link_, item_title_))
+        existing_feed = insert_item(existing_feed, ' '.join(new_items))
     with open("kevin_lewis.rss", "w") as f:
-        f.write(new_feed)
+        f.write(existing_feed)
 
 except FileNotFoundError:
-    basic_feed = create_feed("Kevin Lewis", ROOT + "/authors/detail/kevin-lewis", titles_di)
+    new_feed = create_feed(
+        "Kevin Lewis", ROOT + "/authors/detail/kevin-lewis", titles_di
+    )
     with open("kevin_lewis.rss", "w+") as f:
-        f.write(basic_feed)
+        f.write(new_feed)
